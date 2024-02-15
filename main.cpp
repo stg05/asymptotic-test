6768ffab2baac0eb0b1569b9ca9b982932d4ed43
stg05 <stecenkogeorgii2005@gmail.com>
2024-02-15 23:39:12 +0300

    added jupyter, first runs

--- main.cpp
+++ main.cpp
@@ -1,20 +1,42 @@
 #include <iostream>
 #include <chrono>
+#include <random>
+#include <fstream>
 
 using namespace std;
 
-void func() {
+int *bruteforce(int array[], size_t count, int target) {
+    for (int i = 0; i < count; i++) {
+        if (array[i] == target) {
+            return array + i * sizeof(int);
+        }
+    }
+    return nullptr;
+}
 
-};
+void fill_array(int target[], size_t count) {
+    unsigned seed = 30239;
+    default_random_engine rng(seed);
+    uniform_int_distribution<int> dstr(0, 9);
+    for (unsigned i = 0; i < count; i++) {
+        target[i] = dstr(rng);
+    }
+}
+
+string runTest(size_t count) {
+    int workingArray[count];
+    fill_array(workingArray, count);
+    auto begin = chrono::steady_clock::now();
+    bruteforce(workingArray, count, 10);
+    auto end = std::chrono::steady_clock::now();
+    auto span = chrono::duration_cast<chrono::milliseconds>(end - begin).count();
+    return to_string(count) + ";" + to_string(span) + "\n";
+}
 
 int main() {
-    auto begin = chrono::steady_clock::now();
-    for (unsigned cnt = 100000; cnt != 0; cnt--) {
-        func();
+    std::ofstream out("testdata.csv");
+    for(unsigned size = 1; size<10000; size++){
+        out << runTest(size);
     }
-    auto end = std::chrono::steady_clock::now();
-    auto span = chrono::duration_cast<chrono::milliseconds>(end - begin);
-
-    std::cout << std::endl << span.count() << std::endl;
     return 0;
 }
