db4064de168c172babd1192e37b90c58ec48fcd9
stg05 <stecenkogeorgii2005@gmail.com>
2024-02-15 16:46:05 +0300

    first commit

---
+++ cmake-build-debug/CMakeFiles/asymptotic_test.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/asymptotic_test.dir/main.cpp.obj"
+  "asymptotic_test.exe"
+  "asymptotic_test.exe.manifest"
+  "asymptotic_test.pdb"
+  "libasymptotic_test.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/asymptotic_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
