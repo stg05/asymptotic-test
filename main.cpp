1dea059ed74db7eba9d872b0bc58ae9ee2d7e388
stg05 <stecenkogeorgii2005@gmail.com>
2024-02-15 17:03:44 +0300

    basement

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include <chrono>
+
+using namespace std;
+
+void func() {
+
+};
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    auto begin = chrono::steady_clock::now();
+    for (unsigned cnt = 100000; cnt != 0; cnt--) {
+        func();
+    }
+    auto end = std::chrono::steady_clock::now();
+    auto span = chrono::duration_cast<chrono::milliseconds>(end - begin);
+
+    std::cout << std::endl << span.count() << std::endl;
     return 0;
 }
